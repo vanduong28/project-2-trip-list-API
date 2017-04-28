ddf812e831837e1df7e98a5d3a4e92382a477b62
vanduong28 <vanduong28@gmail.com>
2017-04-28 09:36:33 -0400

    Add delete city script
    Verified deletion of city from Cities table.

--- scripts/cities/delete-city.sh
+++ scripts/cities/delete-city.sh
@@ -1,1 +1,10 @@
-curl --include --request DELETE http://localhost:4741/cities/4
+# curl --include --request DELETE http://localhost:4741/cities/4
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/cities/$ID"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request DELETE \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json"
+echo
